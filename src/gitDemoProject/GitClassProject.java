90fac5fce08cddafced958422c0f6cc7deaf40eb
Lifeisbeautyful <deepakdeep396@gmail.com>
2023-01-29 12:17:38 +0530

    FirstGitClass

---
+++ src/gitDemoProject/GitClassProject.java
@@ -0,0 +1,14 @@
+package gitDemoProject;
+
+public class GitClassProject {
+
+	public static void main(String[] args) {
+		String str="hemanth";
+		
+		if(str=="hemanth") {
+			System.out.println("its is true value");	
+		}
+		else
+			System.out.println("it is false value");
+	}
+}
